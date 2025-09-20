e005642e4d1c3fd23937bb12e91c28e47a5fffa8
Shweta Mhaske <shwetamhaske2006@gmail.com>
2025-09-20 21:49:40 +0530

    Update Temp sum.c

--- Temp sum.c
+++ Temp sum.c
@@ -3,17 +3,17 @@
 void main()
 {
     float A,B,sum;
-    {
+    
        printf("Enter the temp of sencor A\n");
        scanf("%f",&A);
-    }
-    {
+    
+    
        printf("Enter the temp of sencor B\n");
        scanf("%f",&B);
-    }
+    
     sum=A+B;
-    { 
+     
        printf("total temperature of the sncors is: %2f", sum);
-    }
+    
 
 }
