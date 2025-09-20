4a45e2164e874995c86a0d63dbf71aac33eea3a5
Shweta Mhaske <shwetamhaske2006@gmail.com>
2025-09-20 15:33:50 +0530

    Add program to calculate and display the total temperature from two sensors

---
+++ Temp sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+    float A,B,sum;
+    {
+       printf("Enter the temp of sencor A\n");
+       scanf("%f",&A);
+    }
+    {
+       printf("Enter the temp of sencor B\n");
+       scanf("%f",&B);
+    }
+    sum=A+B;
+    { 
+       printf("total temperature of the ssnsors is: %2f", sum);
+    }
+
+}
