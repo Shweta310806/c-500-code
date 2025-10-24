97989ce7f4e4532a2ddee1620d4fc01406e620d5
Shweta Mhaske <shwetamhaske2006@gmail.com>
2025-10-24 23:14:30 +0530

    Add files via upload

---
+++ Generate diamond number pattern.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    // Upper half of diamond
+    for(i = 1; i <= n; i++) {
+
+        for(j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for(j = 1; j <= i; j++)
+            printf("%d ", j);
+
+        for(j = i - 1; j >= 1; j--)
+            printf("%d ", j);
+
+        printf("\n");
+    }
+
+    // Lower half of diamond
+    for(i = n - 1; i >= 1; i--) {
+
+        for(j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for(j = 1; j <= i; j++)
+            printf("%d ", j);
+
+        for(j = i - 1; j >= 1; j--)
+            printf("%d ", j);
+
+        printf("\n");
+    }
+
+    return 0;
+}
