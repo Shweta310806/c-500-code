277c34da9f3d358999c8bb010b42eb7180a0e42f
Shweta Mhaske <shwetamhaske2006@gmail.com>
2025-10-25 11:55:48 +0530

    Add files via upload

---
+++ Print Pascal triangle using factorial.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int factorial(int n) 
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+        fact *= i;
+    return fact;
+}
+
+int main() 
+{
+    int n, i, j, space;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) 
+    {
+        
+        for (space = 0; space < n - i - 1; space++)
+            printf("  ");
+
+        for (j = 0; j <= i; j++) {
+            int num = factorial(i) / (factorial(j) * factorial(i - j));
+            printf("%4d", num);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
