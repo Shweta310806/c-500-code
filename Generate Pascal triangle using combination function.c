7082f42b2dd903d4df7657096e76dd8fce01e194
Shweta Mhaske <shwetamhaske2006@gmail.com>
2025-10-25 10:43:17 +0530

    Add files via upload

---
+++ Generate Pascal triangle using combination function.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int fact(int n) 
+{
+    int i, f = 1;
+    for(i = 1; i <= n; i++)
+        f = f * i;
+    return f;
+}
+
+int nCr(int n, int r) 
+{
+    return fact(n) / (fact(r) * fact(n - r));
+}
+
+int main() 
+{
+    int rows, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for(i = 0; i < rows; i++) 
+    {
+
+        for(j = 0; j < rows - i; j++)
+            printf(" ");
+
+        for(j = 0; j <= i; j++)
+            printf("%d ", nCr(i, j));
+
+        printf("\n");
+    }
+
+    return 0;
+}
